f38f1ab9b60c546f93fc80d02c974a2718ee1579
yuu5322 <yukah5322@gmail.com>
2022-12-13 14:30:29 +0900

    add generators

---
+++ data/images/model_generator.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+#モデルの構築
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# カテゴリ
+CATEGORIES = [
+    u'16Tea',
+    u'AYATAKA',
+    u'AYATAKA_amami',
+    u'Nagomi',
+    u'OiOcha_shinryoku',
+    u'BarleyTea_Tsurube',
+    u'OiOcha',]
+
+# 密度
+DENSE_SIZE = len(CATEGORIES)
+# 画像サイズ
+IMG_SIZE = 150
+INPUT_SHAPE = (IMG_SIZE, IMG_SIZE,3)
+# 教師データ
+X_TRAIN = []
+Y_TRAIN = []
+# テストデータ
+X_TEST = []
+Y_TEST = []
+# データ保存先
+TRAIN_TEST_DATA = './train_test_data/data.npy'
+# モデル保存先
+MODEL_ROOT_DIR = './models/'
+
+
+# ----- モデル構築 ----- #
+model = keras.models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation="relu",input_shape=INPUT_SHAPE))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation="relu"))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3),activation="relu"))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3),activation="relu"))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512,activation="relu"))
+model.add(layers.Dense(DENSE_SIZE,activation="sigmoid"))
+
+#モデル構成の確認
+model.summary()
+# ----- /モデル構築 ----- #
+
+# ----- モデルコンパイル ----- #
+model.compile(loss="binary_crossentropy",
+              optimizer=keras.optimizers.RMSprop(lr=1e-4),
+              metrics=["acc"])
+# ----- /モデル構築 ----- #
+
+# ----- モデル学習 ----- #
+# 教師データとテストデータを読み込む
+X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = np.load(TRAIN_TEST_DATA, allow_pickle=True)
+model = model.fit(X_TRAIN,
+                  Y_TRAIN,
+                  epochs=10,
+                  batch_size=6,
+                  validation_data=(X_TEST, Y_TEST))
+# ----- /モデル学習 ----- #
+
+# ----- 学習結果プロット ----- #
+acc = model.history['acc']
+val_acc = model.history['val_acc']
+loss = model.history['loss']
+val_loss = model.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.savefig(os.path.join(MODEL_ROOT_DIR, 'Training_and_validation_accuracy.png'))
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.savefig(os.path.join(MODEL_ROOT_DIR, 'Training_and_validation_loss.png'))
+# ----- /学習結果プロット ----- #
+
+# ----- モデル保存 ----- #
+# モデル保存
+json_string = model.model.to_json()
+open(os.path.join(MODEL_ROOT_DIR, 'model_predict.json'), 'w').write(json_string)
+
+#重み保存
+model.model.save_weights(os.path.join(MODEL_ROOT_DIR, 'model_predict.hdf5'))
+# ----- /モデル保存 ----- #
---
+++ data/images/train_test_data_generator.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+import os, glob
+import numpy as np
+import random, math
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# 画像が保存されているルートディレクトリのパス
+IMG_ROOT_DIR = './images/extended'
+# カテゴリ
+CATEGORIES = [
+    u'16Tea',
+    u'AYATAKA',
+    u'AYATAKA_amami',
+    u'Nagomi',
+    u'OiOcha_shinryoku',
+    u'BarleyTea_Tsurube',
+    u'OiOcha',]
+
+# 密度
+DENSE_SIZE = len(CATEGORIES)
+# 画像サイズ
+IMG_SIZE = 150
+# 画像データ
+X = []
+# カテゴリデータ
+Y = []
+# 教師データ
+X_TRAIN = []
+Y_TRAIN = []
+# テストデータ
+X_TEST = []
+Y_TEST = []
+# 生成するファイルの名前
+TRAIN_TEST_DATA = './train_test_data/data.npy'
+
+
+# カテゴリごとに処理する
+# enumerateはリストのインデックス番号と要素の両方を取得できるようにするやつ
+for idx, category in enumerate(CATEGORIES):
+    # 各ラベルの画像ディレクトリにアクセス
+    image_dir = os.path.join(IMG_ROOT_DIR, category)
+    #.jpgで終わるファイルを全部取得してfilesの中に入れる
+    files = glob.glob(os.path.join(image_dir, '*.jpg'))
+    #filesでforループ
+    for f in files:
+        # 各画像をリサイズしてデータに変換する
+        img = Image.open(f)
+        img = img.convert('RGB')
+        img = img.resize((IMG_SIZE, IMG_SIZE))
+        data = np.asarray(img)
+        # appendはリストに要素を追加するやつ
+        X.append(data)
+        Y.append(idx)
+
+X = np.array(X)
+Y = np.array(Y)
+
+# 正規化
+X = X.astype('float32') /255
+#one-hotベクトルにする、DENSE_SIZEが最大値
+Y = keras.utils.to_categorical(Y, DENSE_SIZE)
+
+# 教師データとテストデータを分ける
+X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, test_size=0.20)
+
+# 教師／テストデータを保存する
+np.save(TRAIN_TEST_DATA, (X_TRAIN, X_TEST, Y_TRAIN, Y_TEST))
+print(u'教師／テストデータの作成が完了しました。: {}'.format(TRAIN_TEST_DATA))
