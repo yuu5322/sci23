ed232d990c42f52af3734c319f44168087cc3aff
yuu5322 <yukah5322@gmail.com>
2023-05-18 18:21:39 +0900

    パスを通して動くようにする

--- data/face_color.py
+++ data/face_color.py
@@ -4,11 +4,12 @@ import os
 import uuid
 import glob
 
-face_cut_imgs_dir_path = os.path.join(os.getcwd(),"data/face_cut_imgs/")
-face_color_imgs_dir_path = os.path.join(os.getcwd(),"data/face_color_imgs/")
+face_cut_imgs_dir = os.path.join(os.getcwd(),"images/face_cut")
+face_color_imgs_dir = os.path.join(os.getcwd(),"images/face_color/")
+os.makedirs(face_color_imgs_dir, exist_ok=True)
 
-#data/face_cut_imgs内の画像を全て読み込む
-files = glob.glob(str(face_cut_imgs_dir_path) + str("*"))
+#face_cutディレクトリ内の画像を全て読み込む
+files = glob.glob(os.path.join(face_cut_imgs_dir, '*.jpeg'))
 
 # define parameter
 HSV_MIN = np.array([0, 30, 70])
@@ -22,11 +23,13 @@ for f in files:
     img = cv2.imread(f)
 
     #convert hsv
+    #rgb画像をhsv画像に変更
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     #mask hsv region
+    #肌色領域をマスク
     mask_hsv = cv2.inRange(img_hsv, HSV_MIN, HSV_MAX)
 
     # save image
-    # cv2.imwrite("mask_hsv.jpg", mask_hsv)
-    cv2.imwrite(face_color_imgs_dir_path + str(uuid.uuid4()) + str('.jpeg'), mask_hsv)
+    #print(face_color_imgs_dir + str(uuid.uuid4()) + str('.jpeg'))
+    cv2.imwrite(face_color_imgs_dir + str(uuid.uuid4()) + str('.jpeg'), mask_hsv)
