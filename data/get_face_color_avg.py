da23afa27d73126a1f2c513aaa88db6fb4b25a36
yuu5322 <yukah5322@gmail.com>
2023-05-29 17:13:26 +0900

    fix errors

--- data/get_face_color_avg.py
+++ data/get_face_color_avg.py
@@ -1,24 +1,51 @@
 # python get_avg_face_color.py -h "ファイルのパス"
 
-#import argparse
 import numpy as np
 import cv2
 import os
 import glob
-# HSV_MIN = np.array([0, 30, 70])
-# HSV_MAX = np.array([20, 150, 255])
 
 # パスの定義
 # 画像のルートディレクトリ
-img_root_dir = './images/'
+current_dir = os.getcwd()
+img_root_dir = os.path.join(current_dir, 'images/')
 # 顔だけを切り抜いた画像のパス
 face_cut_dir = os.path.join(img_root_dir, 'face_cut')
+# マスク後の画像の保存場所
+masked_imgs_dir = os.path.join(img_root_dir, 'face_masked')
+os.makedirs(masked_imgs_dir, exist_ok=True)
 
 # 顔画像（カラー）の読み込み
 color_files = glob.glob(os.path.join(face_cut_dir, '*.jpeg'))
-print(color_files)
 
-img_AND = cv2.bitwise_and(img, mask)
+for f in color_files:
+
+    color_img = cv2.imread(f)
+
+    try:
+        # 処理している画像と同じ名前のマスク画像を読み込む
+        mask_img_path = f.replace('./images/face_cut/', './images/face_color/')
+        mask_img = cv2.imread(mask_img_path)
+        # そのままだと画像サイズが違ってエラーが出るので、グレースケールに変換
+        #mask_img_gray = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
+
+        # 画像をマスクして保存
+        img_AND = cv2.bitwise_and(color_img, mask_img)
+        file_name = f.replace('./images/face_cut/', './images/face_masked/')
+        cv2.imwrite(file_name, img_AND)
+
+    except Exception as e:
+        print(f)
+        print(e)
+
+
+
+
+
+
+
+
+
 
 # def parse_args():
 #     parser = argparse.ArgumentParser(description="get averaged HSV color for image file(s)")
