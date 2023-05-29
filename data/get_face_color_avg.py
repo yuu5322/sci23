991a06c909a1ff19d47b3e1222ec7a150a5a6f1b
yuu5322 <yukah5322@gmail.com>
2023-05-29 15:47:55 +0900

    add commentout

--- data/get_face_color_avg.py
+++ data/get_face_color_avg.py
@@ -1,38 +1,40 @@
 # python get_avg_face_color.py -h "ファイルのパス"
 
-import argparse
+#import argparse
 import numpy as np
 import cv2
 HSV_MIN = np.array([0, 30, 70])
 HSV_MAX = np.array([20, 150, 255])
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="get averaged HSV color for image file(s)")
-    parser.add_argument("FILES", type=str, nargs="+", help="image file(s)")
-    return parser.parse_args()
+img_AND = cv2.bitwise_and(img, mask)
 
-def proc_file(file):
-    img = cv2.imread(file)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask_bin = cv2.inRange(img_hsv, HSV_MIN, HSV_MAX) / 255
-    mask_bin = mask_bin.ravel().reshape(-1, 1)
-    mask_bin = np.repeat(mask_bin, img_hsv.shape[2], axis=1)
-    img_hsv = img_hsv.reshape(-1, 3)
-    sum_hsv = np.sum(img_hsv * mask_bin, axis=0)
-    sum_bin = np.sum(mask_bin, axis=0)
-    return sum_hsv / sum_bin
+# def parse_args():
+#     parser = argparse.ArgumentParser(description="get averaged HSV color for image file(s)")
+#     parser.add_argument("FILES", type=str, nargs="+", help="image file(s)")
+#     return parser.parse_args()
 
-def main(args):
-    avg = {}
-    for f in args.FILES:
-        avg[f] = proc_file(f)
-    for f in avg.keys():
-        print(f)
-        # print(avg[f])
-        val = avg[f]
-        for v in val:
-            print(v)
+# def proc_file(file):
+#     img = cv2.imread(file)
+#     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#     mask_bin = cv2.inRange(img_hsv, HSV_MIN, HSV_MAX) / 255
+#     mask_bin = mask_bin.ravel().reshape(-1, 1)
+#     mask_bin = np.repeat(mask_bin, img_hsv.shape[2], axis=1)
+#     img_hsv = img_hsv.reshape(-1, 3)
+#     sum_hsv = np.sum(img_hsv * mask_bin, axis=0)
+#     sum_bin = np.sum(mask_bin, axis=0)
+#     return sum_hsv / sum_bin
 
-if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+# def main(args):
+#     avg = {}
+#     for f in args.FILES:
+#         avg[f] = proc_file(f)
+#     for f in avg.keys():
+#         print(f)
+#         # print(avg[f])
+#         val = avg[f]
+#         for v in val:
+#             print(v)
+
+# if __name__ == "__main__":
+#     args = parse_args()
+#     main(args)
