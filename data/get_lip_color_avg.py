1bce2952d432d473edd5bb06bb3840681dd3f699
yuu5322 <yukah5322@gmail.com>
2023-10-16 12:58:24 +0900

    line 32: IndexError: list index out of range出る

--- data/get_lip_color_avg.py
+++ data/get_lip_color_avg.py
@@ -15,45 +15,66 @@ face_cut_dir = os.path.join(img_root_dir, 'face_cut')
 lip_cut_dir = os.path.join(img_root_dir, 'lip_cut')
 os.makedirs(lip_cut_dir, exist_ok=True)
 
-# img1: 元の画像（とりあえず画像を1枚指定）
-img1 = np.array(Image.open(os.path.join(face_cut_dir, '0a6d8bbe-bff4-4e44-afe1-730d7161992c.jpeg')))
-# 画像データをRGBからBGRへ変換
-img1 = img1[:, :, ::-1]
-
-# contour: 切り抜きたい形の輪郭の頂点情報, 形状が (NumPoints, 2) の numpy 配列
-# contour = np.array((( 67, 148),
-# ( 73, 140),
-# ( 83, 134),
-# ( 92, 135),
-# (101, 131),
-# (117, 132),
-# (134, 134),
-# (123, 150),
-# (110, 159),
-# (100, 161),
-# ( 90, 162)))
-
-# 切り抜きたい座標を取得
+# 顔画像（カラー）の読み込み
+face_imgs = glob.glob(os.path.join(face_cut_dir, '*.jpeg'))
+# 切り抜きたい座標が入っているcsvを取得
 df = pd.read_csv(os.path.join(current_dir, 'csv/lip_landmarks.csv'))
-#　画像ファイル名と一致する座標を取り出す
-lip_landmark = df[df['file_name']==os.path.join(face_cut_dir, '0a6d8bbe-bff4-4e44-afe1-730d7161992c.jpeg')]['lip_landmarks']
-# \nなどの余分なものを取り除く
-b = [list(map(int, s.split())) for s in [t for t in lip_landmark.str.replace("\\n", "").str.replace("]","").str.split("[") if t][0] if s]
-#　arrayに変換
-contour = np.array(b)
+
+for f in face_imgs:
+
+    img = np.array(Image.open(f))
+    # 画像データをRGBからBGRへ変換
+    img_bgr = img[:, :, ::-1]
+
+    #　画像ファイル名と一致する座標を取り出す
+    lip_landmark = df[df['file_name']==os.path.join(f)]['lip_landmarks']
+    # \nなどの余分なものを取り除く
+    b = [list(map(int, s.split())) for s in [t for t in lip_landmark.str.replace("\\n", "").str.replace("]","").str.split("[") if t][0] if s]
+    #　arrayに変換
+    contour = np.array(b)
+
+    # マスク画像を作成
+    # 元の画像と同じ大きさのマスク画像を作る
+    mask = np.zeros_like(img_bgr)
+    cv2.fillConvexPoly(mask, contour, color=(255, 255, 255))
+
+    # 背景画像
+    bg_color = (0, 0, 0) # 黒
+    bg_img = np.full_like(img_bgr, bg_color)
+
+    # np.where() はマスクの値が (255, 255, 255) の要素は前景画像 img1 の値、
+    # マスクの値が (0, 0, 0) の要素は背景画像 img2 の値を返す。
+    result = np.where(mask==255, img_bgr, bg_img)
+    file_name = f.replace('/images/face_cut/', '/images/lip_cut/')
+    cv2.imwrite(file_name, result)
 
 
-# マスク画像を作成
-# 元の画像と同じ大きさのマスク画像を作る
-mask = np.zeros_like(img1)
-cv2.fillConvexPoly(mask, contour, color=(255, 255, 255))
+# # img1: 元の画像（とりあえず画像を1枚指定）
+# img1 = np.array(Image.open(os.path.join(face_cut_dir, '0a6d8bbe-bff4-4e44-afe1-730d7161992c.jpeg')))
+# # 画像データをRGBからBGRへ変換
+# img1 = img1[:, :, ::-1]
 
-# 背景画像
-bg_color = (0, 0, 0) # 黒
-img2 = np.full_like(img1, bg_color)
+# # 切り抜きたい座標を取得
+# df = pd.read_csv(os.path.join(current_dir, 'csv/lip_landmarks.csv'))
+# #　画像ファイル名と一致する座標を取り出す
+# lip_landmark = df[df['file_name']==os.path.join(face_cut_dir, '0a6d8bbe-bff4-4e44-afe1-730d7161992c.jpeg')]['lip_landmarks']
+# # \nなどの余分なものを取り除く
+# b = [list(map(int, s.split())) for s in [t for t in lip_landmark.str.replace("\\n", "").str.replace("]","").str.split("[") if t][0] if s]
+# #　arrayに変換
+# contour = np.array(b)
 
-# np.where() はマスクの値が (255, 255, 255) の要素は前景画像 img1 の値、
-# マスクの値が (0, 0, 0) の要素は背景画像 img2 の値を返す。
-result = np.where(mask==255, img1, img2)
-file_name = '0a6d8bbe-bff4-4e44-afe1-730d7161992c.jpeg'
-cv2.imwrite(os.path.join(lip_cut_dir, file_name), result)
+
+# # マスク画像を作成
+# # 元の画像と同じ大きさのマスク画像を作る
+# mask = np.zeros_like(img1)
+# cv2.fillConvexPoly(mask, contour, color=(255, 255, 255))
+
+# # 背景画像
+# bg_color = (0, 0, 0) # 黒
+# img2 = np.full_like(img1, bg_color)
+
+# # np.where() はマスクの値が (255, 255, 255) の要素は前景画像 img1 の値、
+# # マスクの値が (0, 0, 0) の要素は背景画像 img2 の値を返す。
+# result = np.where(mask==255, img1, img2)
+# file_name = '0a6d8bbe-bff4-4e44-afe1-730d7161992c.jpeg'
+# cv2.imwrite(os.path.join(lip_cut_dir, file_name), result)
