078dcc80f8171fa16d5d886274a4a282bb2f6dd7
yuu5322 <yukah5322@gmail.com>
2023-10-16 14:44:22 +0900

    csvファイルに色の平均値を出力

--- data/get_lip_color_avg.py
+++ data/get_lip_color_avg.py
@@ -49,4 +49,48 @@ for f in face_imgs:
         result = np.where(mask==255, img_bgr, bg_img)
         file_name = f.replace('/images/face_cut/', '/images/lip_cut/')
         cv2.imwrite(file_name, result)
-        
+
+
+# 唇領域の色の平均値の計算
+
+# マスク済み画像の読み込み
+lip_imgs = glob.glob(os.path.join(lip_cut_dir, '*.jpeg'))
+
+#変数定義
+input_dir = lip_cut_dir
+output_dir = os.path.join(current_dir, 'csv/')
+num_photo = sum(os.path.isfile(os.path.join(lip_cut_dir, name)) for name in os.listdir(lip_cut_dir))
+#numpy配列で定義すると1種類のデータ型しか追加できなくなるので、空の配列で定義する
+bgr = []
+#csvを出力する時の通し番号
+file_number = 0
+
+for k in lip_imgs:
+    img = cv2.imread(k)
+    h, w, c = img.shape #height, width, channnel
+
+    #初期化
+    l=0
+    b_ave=0; g_ave=0; r_ave=0
+
+    for i in range(h):
+        for j in range(w):
+            #画素値[0,0,0]（Black）を除外してピクセルの和とbgrの画素値の合計を計算する
+            if(img[i,j,0] != 0 or img[i,j,1] != 0 or img[i,j,2] != 0 ):
+                l+=1    #対象となるピクセル数を計算する
+                #対象となるピクセルの画素値の和を計算する
+                b_ave=b_ave+img[i,j,0]
+                g_ave=g_ave+img[i,j,1]
+                r_ave=r_ave+img[i,j,2]
+
+    #画素値合計をピクセル数で除することでRGBの画素値の平均値を求める
+    b_ave=b_ave/l
+    g_ave=g_ave/l
+    r_ave=r_ave/l
+
+    #np配列だった場合はbgr[file_number]=np.array([file_number, b_ave, g_ave, r_ave])
+    bgr.append([file_number, k, b_ave, g_ave, r_ave])
+    file_number = file_number + 1
+
+df = pd.DataFrame(bgr, columns=['file_number', 'filename', 'blue', 'green', 'red'])    #opencvの並び準BGRに合わせる
+df.to_csv(output_dir + '/lip_color_avg.csv')
