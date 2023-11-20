854317c90b15eed2b03a493c640e9983ff80e3f4
yuu5322 <yukah5322@gmail.com>
2023-11-20 16:42:18 +0900

    fix memo

--- data/get_thumbnail_images.py
+++ data/get_thumbnail_images.py
@@ -6,7 +6,7 @@ import re
 import uuid
 from bs4 import BeautifulSoup
 
-# 200ページ分（1ページは約12記事）のサムネイル画像を取得
+# 200ページ分（1ページは約12記事）、合計2400枚のサムネイル画像を取得
 # 多分1時間弱くらいかかった
 for page in range(1,201):
    r = requests.get("https://maquia.hpplus.jp/makeup/news/?page="+str(page))
