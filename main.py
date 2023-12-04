32d6ebb09cb503abe829b8721393118913eb2c5a
yuu5322 <yukah5322@gmail.com>
2023-12-04 14:23:42 +0900

    fix path

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import os
 
 # パスの整理
 current_dir = os.getcwd()
-MODEL_PATH = os.path.join(current_dir, '/data/models/model_predict.json')
-WEIGHT_PATH = os.path.join(current_dir, '/data/models/model_predict.hdf5')
+MODEL_PATH = os.path.join(current_dir, 'data/models/model_predict.json')
+WEIGHT_PATH = os.path.join(current_dir, 'data/models/model_predict.hdf5')
 
 # 画像サイズ
 IMG_SIZE = 150
@@ -24,20 +24,20 @@ model.load_weights(WEIGHT_PATH)
 
 
 # # 入力引数から画像を読み込む
-# args = sys.argv
-# img = keras.preprocessing.image.load_img(args[1], target_size=INPUT_SHAPE)
-# x = keras.preprocessing.image.img_to_array(img)
-# x = np.expand_dims(x, axis=0)
+args = sys.argv
+img = keras.preprocessing.image.load_img(args[1], target_size=INPUT_SHAPE)
+x = keras.preprocessing.image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
 
 # # モデルで予測する
-# features = model.predict(x)
-# print("確率：")
-# for i in range(0, 7):
-#     print(str(CATEGORIES_NAME[i]) + ' ： ' + str(features[0][i]))
-# print("----------------------------------------------")
-# print("計算結果")
-# if np.argmax(features[0]) == 1:
-#     print(u'選ばれたのは綾鷹でした。')
-# else:
-#     print(u'綾鷹ではなく' + str(CATEGORIES_NAME[np.argmax(features[0])]) + 'が選ばれました。')
-# print("----------------------------------------------")
+features = model.predict(x)
+print("確率：")
+for i in range(0, 7):
+    print(str(CATEGORIES_NAME[i]) + ' ： ' + str(features[0][i]))
+print("----------------------------------------------")
+print("計算結果")
+if np.argmax(features[0]) == 1:
+    print(u'選ばれたのは綾鷹でした。')
+else:
+    print(u'綾鷹ではなく' + str(CATEGORIES_NAME[np.argmax(features[0])]) + 'が選ばれました。')
+print("----------------------------------------------")
