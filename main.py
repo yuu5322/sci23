6e100f02d7cb47828e4a01885f57d264122c45b9
yuu5322 <yukah5322@gmail.com>
2023-12-15 12:02:05 +0900

    顔領域を切り抜いて保存する

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-# $ python main.py './data/images/test/test1.jpeg'
+# $ python main.py './test/test1.jpeg'
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
 import numpy as np
 import sys
 import os
+import cv2
 
 # パスの整理
 current_dir = os.getcwd()
 MODEL_PATH = os.path.join(current_dir, 'data/models/model_predict.json')
 WEIGHT_PATH = os.path.join(current_dir, 'data/models/model_predict.hdf5')
+# 入力引数から画像を読み込む
+args = sys.argv
 
+# 顔領域の切り取り
+#顔検出器のインポート
+import dlib
+faceDetector = dlib.get_frontal_face_detector()
+#顔を切り抜く関数をdefで定義
+def crop_face(path):
+    img = cv2.imread(path)
+    faces = faceDetector(img, 0)
+    if len(faces) > 0:
+        for i in range(0, len(faces)):# face_img には i 番目の顔のみが入る（他の顔の情報は無視される）
+            face_img = img[int(faces[i].top()):int(faces[i].bottom()),int(faces[i].left()):int(faces[i].right())]
+    return face_img
+
+f = args[1]
+try:
+    face_img = crop_face(f)
+    file_name = f.replace('test1', 'face_cut1')
+    cv2.imwrite(file_name, face_img)
+except Exception as e:
+    print(f)
+    print(e)
+
+
+'''
 # 画像サイズ
 IMG_SIZE = 150
 INPUT_SHAPE = (IMG_SIZE, IMG_SIZE,3)
@@ -23,8 +49,6 @@ model.load_weights(WEIGHT_PATH)
 
 
 
-# 入力引数から画像を読み込む
-args = sys.argv
 # 読み込んだ画像をINPUT_SHAPEにリサイズ
 img = keras.preprocessing.image.load_img(args[1], target_size=INPUT_SHAPE)
 # 前の行でリサイズしたimgをndarrayに変換
@@ -46,3 +70,5 @@ print(features)
 # else:
 #     print(u'綾鷹ではなく' + str(CATEGORIES_NAME[np.argmax(features[0])]) + 'が選ばれました。')
 # print("----------------------------------------------")
+
+'''
